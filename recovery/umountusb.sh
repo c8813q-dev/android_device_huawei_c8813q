5355be7b330746fc4df1ecf6b42ef66129405f64
xiaognol <317065820@qq.com>
2014-05-30 03:28:54 +0800

    add umountusb script

---
+++ recovery/umountusb.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+#test.sh
+echo "0" > /sys/class/android_usb/android0/f_mass_storage/lun0/file
+echo "0" > /sys/class/android_usb/android0/f_mass_storage/lun1/file
+echo "0" > /sys/class/android_usb/android0/f_mass_storage/lun1/file
