a9aee798541444b3dc9e570ac10d540cf7987d6c
xiaognol <317065820@qq.com>
2014-05-30 03:31:37 +0800

    fix umountusb script

--- recovery/umountusb.sh
+++ recovery/umountusb.sh
@@ -1,5 +1,5 @@
-#!bin/bash
-#test.sh
+#!/sbin/sh
+#
 echo "0" > /sys/class/android_usb/android0/f_mass_storage/lun0/file
 echo "0" > /sys/class/android_usb/android0/f_mass_storage/lun1/file
-echo "0" > /sys/class/android_usb/android0/f_mass_storage/lun1/file
+echo "0" > /sys/class/android_usb/android0/f_mass_storage/lun2/file
